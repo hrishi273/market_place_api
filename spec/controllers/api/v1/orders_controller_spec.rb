f4361f6b0dee1e903e9071519f4b443b869db461
hrishi273 <hpk273@gmail.com>
2016-12-03 21:37:20 +0530

    Allows order to be placed along with quantity

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -53,7 +53,7 @@ describe Api::V1::OrdersController do
 
       product_1 = FactoryGirl.create :product
       product_2 = FactoryGirl.create :product
-      order_params = { product_ids: [product_1.id, product_2.id] }
+      order_params = { product_ids_and_quantities: [[product_1.id, 2],[ product_2.id, 3]] }
       post :create, user_id: current_user.id, order: order_params
     end
 
@@ -62,6 +62,13 @@ describe Api::V1::OrdersController do
       expect(order_response[:id]).to be_present
     end
 
+    it "embeds the two product objects related to the order" do
+      order_response = json_response
+      expect(order_response[:products].size).to eql 2
+    end
+
     it { should respond_with 201 }
   end
+
+
 end
