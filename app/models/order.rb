70a6e30711214e802d0a769c2fb080af9b5f6683
hrishi273 <hpk273@gmail.com>
2016-12-03 21:55:23 +0530

    Complete

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,6 +8,7 @@ class Order < ActiveRecord::Base
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
+  validates_with EnoughProductsValidator
 
   def set_total!
     self.total = products.map(&:price).sum
@@ -17,7 +18,7 @@ class Order < ActiveRecord::Base
     product_ids_and_quantities.each do |product_id_and_quantity|
       id, quantity = product_id_and_quantity # [1,5]
 
-      self.placements.build(product_id: id)
+      self.placements.build(product_id: id, quantity: quantity)
     end
   end
 end
